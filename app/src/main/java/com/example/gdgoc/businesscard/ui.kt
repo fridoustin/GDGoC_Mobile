6d840b45530daa994f51a8cfd75b301221be2823
fridoustin <fridolinaustin16@gmail.com>
2025-02-07 00:19:51 +0800

    feat : make business card ui

---
+++ app/src/main/java/com/example/gdgoc/businesscard/ui.kt
@@ -0,0 +1,240 @@
+package com.example.gdgoc.businesscard
+
+import android.widget.Space
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.gdgoc.R
+
+@Composable
+fun BusinessCard(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFF5F5DC)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(330.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.background),
+                contentDescription = "banner",
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                modifier = Modifier
+                    .padding(top = 130.dp)
+                    .align(Alignment.Center)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.chillguy),
+                    contentDescription = "Logo",
+                    modifier = Modifier
+                        .size(180.dp)
+                        .clip(CircleShape)
+                        .border(2.dp, Color.Black, CircleShape),
+                    contentScale = ContentScale.Crop
+                )
+            }
+
+        }
+
+        Spacer(modifier = Modifier.padding(8.dp))
+
+        Text(
+            text = "Fridolin Austin Sulistyo",
+            color = Color.Black,
+            fontSize = 28.sp,
+            fontWeight = FontWeight.ExtraBold
+        )
+        Spacer(modifier = Modifier.padding(4.dp))
+        Text(
+            text = "GDGoC Unhas Core Team - Mobile Technical",
+            color = Color.Gray,
+            fontSize = 16.sp,
+        )
+
+        Spacer(modifier = Modifier.padding(16.dp))
+
+        Card(
+            modifier = Modifier
+                .padding(horizontal = 32.dp),
+            colors = CardDefaults.cardColors(Color.White),
+            elevation = CardDefaults.cardElevation(4.dp),
+            shape = RoundedCornerShape(30.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(32.dp, 24.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Call,
+                    contentDescription = "email",
+                    modifier = Modifier.size(36.dp)
+                )
+
+                Spacer(modifier = Modifier.padding(12.dp))
+
+                Text(
+                    text = "+62081234567890",
+                    fontSize = 20.sp,
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.padding(8.dp))
+
+        Card(
+            modifier = Modifier
+                .padding(horizontal = 32.dp),
+            colors = CardDefaults.cardColors(Color.White),
+            elevation = CardDefaults.cardElevation(4.dp),
+            shape = RoundedCornerShape(30.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(32.dp, 24.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Email,
+                    contentDescription = "email",
+                    modifier = Modifier.size(36.dp)
+                )
+
+                Spacer(modifier = Modifier.padding(12.dp))
+
+                Text(
+                    text = "GDGoCkeren@gmail.com",
+                    fontSize = 18.sp,
+                )
+
+            }
+        }
+
+        Spacer(modifier = Modifier.padding(16.dp))
+
+        Text(
+            text = "Connect with me",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+
+        )
+
+        Spacer(modifier = Modifier.padding(8.dp))
+
+        Card(
+            modifier = Modifier
+                .padding(horizontal = 32.dp),
+            colors = CardDefaults.cardColors(Color.White),
+            elevation = CardDefaults.cardElevation(4.dp),
+            shape = RoundedCornerShape(30.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(32.dp, 24.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ig),
+                    contentDescription = "logo ig",
+                    modifier = Modifier.size(48.dp)
+                )
+
+                Spacer(modifier = Modifier.padding(12.dp))
+
+                Text(
+                    text = "@fridoustin_",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.weight(1f)
+                )
+
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowRight,
+                    contentDescription = "next",
+                    modifier = Modifier.size(48.dp)
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.padding(8.dp))
+
+        Card(
+            modifier = Modifier
+                .padding(horizontal = 32.dp),
+            colors = CardDefaults.cardColors(Color.White),
+            elevation = CardDefaults.cardElevation(4.dp),
+            shape = RoundedCornerShape(30.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(32.dp, 24.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.github),
+                    contentDescription = "logo ig",
+                    modifier = Modifier.size(48.dp)
+                )
+
+                Spacer(modifier = Modifier.padding(12.dp))
+
+                Text(
+                    text = "fridoustin",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.weight(1f)
+                )
+
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowRight,
+                    contentDescription = "next",
+                    modifier = Modifier.size(48.dp)
+                )
+            }
+        }
+    }
+}
+
+//4285f4
